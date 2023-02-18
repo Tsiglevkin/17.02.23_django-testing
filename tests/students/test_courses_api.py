36ee1f712021f2ea2b0e52d9bc3d777c52040ab5
Stepan Tsiglevkin <stepan.tsiglevkin@mail.ru>
2023-02-18 12:15:33 +0500

    fill settings.py, made fixture, made 1st test (doesn't work).

--- tests/students/test_courses_api.py
+++ tests/students/test_courses_api.py
@@ -1,2 +1,48 @@
-def test_example():
-    assert False, "Just test example"
+import pytest
+from django.contrib.auth.models import User
+from django.urls import reverse
+from rest_framework.test import APIClient
+from model_bakery import baker
+
+from students.models import Course, Student
+
+
+@pytest.fixture
+def client():
+    return APIClient()
+
+
+@pytest.fixture
+def user():
+    return User.objects.create_user('тестовый пользователь')
+
+
+@pytest.fixture
+def courses_factory():
+    def factory(*args, **kwargs):
+        return baker.make(Course, *args, **kwargs)
+    return factory
+
+
+@pytest.fixture
+def students_factory():
+    def factory(*args, **kwargs):
+        return baker.make(Student, *args, **kwargs)
+    return factory
+
+
+@pytest.mark.django_db
+def test_get_1st_course(user, client, courses_factory):
+
+    # course = courses_factory(_quantity=5)
+    # url = reverse('courses')  # выпадает ошибка использования функции - не найдено имя ссылки
+
+    response = client.get('/courses/')
+
+    assert response.status_code == 200  # тест не проходит - статус 404.
+
+    # data = response.json()
+    # request_course = data.get('name')
+    #
+    # assert request_course == course[0].name
+    # print(request_course, course[0].name)
